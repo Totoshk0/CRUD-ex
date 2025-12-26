aeb8469f47d4e1a5955ecd1642b2674b1c122fe9
Totoshk0 <Fatkulinsasha@gmail.com>
2025-12-26 10:43:12 +0300

    5

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status']
